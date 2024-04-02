0ff013538b8278c2bf1b8e56af54b1ea3a29098d
Yasmin <kodjabashevayasemin@gmail.com>
2024-04-02 09:27:54 +0300

    Refactor navigation bar

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,21 +1,16 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <p>Bulls and Cows</p>
-      
-            <a href="/home" class="text-dark text-decoration-none  btn btn-sm btn">Home</a>
+<nav class="navbar navbar-expand-lg  bg-info ">
+    <div class="d-flex w-100 justify-content-between align-items-center p-2">
+        <h3 class="text-white">Bulls and Cows</h3>
+        <div class="w-25">
+            <a href="/home" class="text-decoration-none  btn btn-sm btn-light">Home</a>
+            <a href="/ranking" class="text-decoration-none  btn btn-sm btn-light">Ranking</a>
+        </div>
 
-
-            <a href="/ranking" class="text-dark text-decoration-none  btn btn-sm btn">Ranking</a>
-
-
-    </div>
-    
-    <div class="container-fluid">
-        <form action="/logout" method="POST" class="d-inline">
-            @csrf
-            <button class="btn btn-sm btn-secondary">Sign Out</button>
-          </form>
-    </div>
-    
+        <div>
+            <form action="/logout" method="POST" class="d-inline">
+                @csrf
+                <button class="btn btn-sm btn-secondary">Sign Out</button>
+            </form>
+        </div>
     </div>
 </nav>
