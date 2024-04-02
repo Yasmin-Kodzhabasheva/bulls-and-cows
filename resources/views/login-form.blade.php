42dcada60632658d6edfd88513ba841c683143f8
Yasmin <kodjabashevayasemin@gmail.com>
2024-04-02 14:28:27 +0300

    Refactor the login form view

--- resources/views/login-form.blade.php
+++ resources/views/login-form.blade.php
@@ -1,34 +1,27 @@
 <x-layout>
+    <x-navigation></x-navigation>
     <div class="d-flex flex-column align-items-center justify-content-center" style="height: 100vh">
-        <h2>Login</h2>
-        <form action="/login" method="POST" id="login-form" class="w-auto">
-            @csrf
-            <div class="form-group">
-                <label for="username" class="text-muted mb-1"><small>Username</small></label>
-                <input name="username" id="username" class="form-control" type="text" autocomplete="off" />
-                @error('username')
-                    <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
-                @enderror
-            </div>
 
-            <div class="form-group">
-                <label for="password" class="text-muted mb-1"><small>Password</small></label>
-                <input name="password" id="password" class="form-control" type="password" autocomplete="off" />
-                @error('password_confirmation')
-                    <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
-                @enderror
-            </div>
+        <div class="d-flex flex-column gap-3 bg-info p-3 rounded">
+            <h2 class="text-white text-center">Login</h2>
+            <form action="/login" method="POST" id="login-form" class="w-auto">
+                @csrf
+                <div class="form-group">
+                    <label for="username" class="text-white mb-1"><small>Username</small></label>
+                    <input name="username" id="username" class="form-control" type="text" autocomplete="off" />
+                </div>
 
-            <button type="submit" class="mt-4 btn btn-success">Login</button>
-        </form>
+                <div class="form-group">
+                    <label for="password" class="text-white mb-1"><small>Password</small></label>
+                    <input name="password" id="password" class="form-control" type="password" autocomplete="off" />
+                </div>
 
-        <div class="d-flex gap-5 m-4">
-            <div>
-                <a href="/register-form" class="text-decoration-none btn btn-primary">Register</a>
-            </div>
-            <div>
-                <a href="/" class="text-decoration-none btn btn-primary">Back</a>
-            </div>
+                <button type="submit" class="mt-4 btn btn-light">Login</button>
+            </form>
+
+            <p class="text-white">Haven't registered yet? Do it
+                <a href="/register-form" class="text-white">here</a>
+            </p>
 
         </div>
     </div>
